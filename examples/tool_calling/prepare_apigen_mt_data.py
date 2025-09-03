d3a4611604c74c331df202d62c502aa057c8ddbf
1stprinciple <ztyharbin@gmail.com>
2025-09-03 18:43:22 +0200

    prepare dataset

--- examples/tool_calling/prepare_apigen_mt_data.py
+++ examples/tool_calling/prepare_apigen_mt_data.py
@@ -1,17 +1,49 @@
 import json
 
-from datasets import load_dataset
+import datasets
+from typing import Any
 
 from rllm.data.dataset import DatasetRegistry
 
+from rllm.parser import get_tool_parser
+from rllm.parser.tool_parser.tool_parser_base import ToolParser
 
-def prepare_apigen_mt_data(train_size: int = None, test_size: int = None):
-    train_dataset = load_dataset("/datasets/train/", name="lcbv5", split="train")
-    test_dataset = load_dataset("/datasets/test/", name="lcbv5", split="test")
 
-    def preprocess_fn(messages, idx):
-        return {"prompt": question, "ground_truth": tests, "data_source": "apigen_mt_5k", "uid": f"apigen_mt_5k_{idx}", "index": idx}
 
+def remove_nulls_recursive(value: Any) -> Any:
+    if isinstance(value, dict):
+        return {k: remove_nulls_recursive(v) for k, v in value.items() if v is not None}
+    elif isinstance(value, list):
+        return [remove_nulls_recursive(x) for x in value]
+    return value
+
+def prepare_apigen_mt_data(train_size: int = None, test_size: int = None, parser_name: str = "qwen"):
+    train_dataset = datasets.load_from_disk("/Users/tianyi/dataset/data/")
+    test_dataset = datasets.load_from_disk("/Users/tianyi/data/")
+
+    parser_class: type[ToolParser] = get_tool_parser(parser_name=parser_name)
+    tool_parser = parser_class()
+
+    def preprocess_fn(example, idx):
+        messages = example["messages"]
+        messages = remove_nulls_recursive(messages)
+        tools = example.get("tools", [])
+        ground_truth = messages[-1]['tool_calls']
+        tools_prompt = tool_parser.get_tool_prompt(json.dumps(tools))
+
+        possible_system_message = messages[0]
+        if possible_system_message["role"] == "system":
+            system_content = possible_system_message["content"]
+            messages[0]["content"] = system_content + tools_prompt
+        else:
+            system_message = {"role": "system", "content": system_content}
+            messages = [system_message] + messages
+
+        return {
+            "prompt": json.dumps(messages[:-1]),
+            "ground_truth": json.dumps(ground_truth),
+            "data_source": "apigen_mt",
+        }
     if train_size:
         train_dataset = train_dataset.select(range(min(train_size, len(train_dataset))))
     if test_size:
@@ -20,10 +52,8 @@ def prepare_apigen_mt_data(train_size: int = None, test_size: int = None):
     train_dataset = train_dataset.map(preprocess_fn, with_indices=True, writer_batch_size=10, num_proc=16)
     test_dataset = test_dataset.map(preprocess_fn, with_indices=True, writer_batch_size=10, num_proc=16)
 
-
-    train_dataset = DatasetRegistry.register_dataset("deepcoder", train_dataset, "train")
-    test_dataset = DatasetRegistry.register_dataset("deepcoder", test_dataset, "test")
-
+    train_dataset = DatasetRegistry.register_dataset("apigen_mt", train_dataset, "train")
+    test_dataset = DatasetRegistry.register_dataset("apigen_mt", test_dataset, "test")
     return train_dataset, test_dataset
 
 
@@ -31,4 +61,4 @@ if __name__ == "__main__":
     train_dataset, test_dataset = prepare_apigen_mt_data(train_size=1000, test_size=20)
     print(f"  - Train dataset: {len(train_dataset.get_data())} examples")
     print(f"  - Test dataset: {len(test_dataset.get_data())} examples")
-    print(train_dataset.get_data()[0])
+    # print(train_dataset.get_data()[0])
