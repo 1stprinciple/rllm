b1b52600fd82c21074942abd2ee3925ff97d2c6c
1stprinciple <ztyharbin@gmail.com>
2025-09-05 05:37:27 +0200

    same project_name

--- examples/tool_calling/train_apigen_mt_16k.sh
+++ examples/tool_calling/train_apigen_mt_16k.sh
@@ -59,7 +59,7 @@ python3 -m examples.tool_calling.train_apigen_mt \
     algorithm.clip_advantages=False \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
-    trainer.project_name='rllm-apigen_mt_8b_16k' \
+    trainer.project_name='rllm-apigen_mt_16k' \
     trainer.experiment_name='rllm-apigen-mt-16k-8b-stage1' \
     trainer.val_before_train=True \
     trainer.n_gpus_per_node=8 \
